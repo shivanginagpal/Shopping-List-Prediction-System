4dfc779e3ab8e9d133b14a81d1f497c27bd96e7d
Megha nair <nairmegha08@gmail.com>
2020-09-26 19:53:53 -0700

    adding items

---
+++ Backend/models/Item.js
@@ -0,0 +1,13 @@
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema
+
+var ItemSchema = new Schema({
+    name :String,
+    category:String,
+    description:String,
+    product_id:Number,
+
+});
+
+module.exports = mongoose.model('Item',ItemSchema)
